519df9c3b2350e926e227f52a2914fe171d0a147
Colin Kinz-Thompson <cdk2119@columbia.edu>
2020-02-18 20:01:39 -0500

    initial commit of new website

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+  $('body').scrollspy({ target: '.navbar',offset: $('.navbar-nav').height()});
+
+  $('.nav-link').on('click', function(){
+    var clickedItem = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(clickedItem).offset().top - $('.navbar-nav').height()
+    }, 500);
+  });
+
+	$('.btn').on('click', function(){
+		var clickedItem = $(this).attr('href');
+		$('html, body').animate({
+				scrollTop: $(clickedItem).offset().top - $('.navbar-nav').height() - 20
+		}, 500);
+	});
+
+	$('.navbar-nav>li>a').on('click', function(){
+			$('.navbar-collapse').collapse('hide');
+	});
+
+	$('.collapse').collapse()
+});
