0f6224dd5bc65759a5652608cd5d96b2e0789580
Colin Kinz-Thompson <cdk2119@columbia.edu>
2020-03-04 12:05:59 -0500

    address offsets

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function (){
-  $('body').scrollspy({ target: '.navbar',offset: $('.navbar-nav').height()});
+  $('body').scrollspy({ target: '.navbar',offset: $('.navbar-nav').height()+50});
 
   $('.nav-link').on('click', function(){
     var clickedItem = $(this).attr('href');
     $('html, body').animate({
-        scrollTop: $(clickedItem).offset().top - $('.navbar-nav').height()
+        scrollTop: $(clickedItem).offset().top - $('.navbar-nav').height()*1.15
     }, 500);
   });
 
 	$('.btn').on('click', function(){
 		var clickedItem = $(this).attr('href');
 		$('html, body').animate({
-				scrollTop: $(clickedItem).offset().top - $('.navbar-nav').height() - 20
+				scrollTop: $(clickedItem).offset().top - $('.navbar-nav').height()*1.15 - 50
 		}, 500);
 	});
 
@@ -19,5 +19,7 @@ $(document).ready(function (){
 			$('.navbar-collapse').collapse('hide');
 	});
 
-	// $('.collapse').collapse()
+	$('.collapse').collapse()
+
 });
+$('.navbar-collapse').collapse('hide')
