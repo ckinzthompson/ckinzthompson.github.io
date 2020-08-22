c6028d88a18ca9f137190c4147d41d8073d0f819
Colin Kinz-Thompson <cdk2119@columbia.edu>
2020-08-22 18:44:03 -0400

    initial breakup into multiple pages

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+document.write('\
+	<div class="container">\
+		CKT - 2020\
+	</div>\
+')
---
+++ js/header.js
@@ -0,0 +1,34 @@
+document.write('\
+<!-- navbar ------------------------------------------------------- -->\
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top py-1">\
+		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsstuff" aria-controls="navbarsstuff" aria-expanded="false" aria-label="Toggle navigation">\
+			<span class="navbar-toggler-icon"></span>\
+		</button>\
+\
+		<div class="navbar-collapse collapse" id="navbarsstuff" style="">\
+			<ul class="navbar-nav mr-auto">\
+				<li class="nav-item">\
+					<a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>\
+				</li>\
+				<li class="nav-item">\
+					<a class="nav-link" href="research.html" >Research</a>\
+				</li>\
+				<li class="nav-item">\
+					<a class="nav-link" href="people.html">People</a>\
+				</li>\
+				<li class="nav-item">\
+					<a class="nav-link" href="publications.html">Publications</a>\
+				</li>\
+				<li class="nav-item">\
+					<a class="nav-link" href="software.html">Software</a>\
+				</li>\
+				<li class="nav-item">\
+					<a class="nav-link" href="./blog">Blog</a>\
+				</li>\
+				<li class="nav-item">\
+					<a class="nav-link" href="contact.html">Contact</a>\
+				</li>\
+			</ul>\
+		</div>\
+	</nav>\
+')
