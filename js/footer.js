78960bc723ad22ea49363040d0f90a0184110415
Colin Kinz-Thompson <colin.kinzthompson@rutgers.edu>
2021-04-18 00:04:18 -0400

    ga

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,15 @@
-// document.write('\
+document.write('\
+<!-- Global site tag (gtag.js) - Google Analytics -->\
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6DXRRLW54Y"></script>\
+<script>\
+  window.dataLayer = window.dataLayer || [];\
+  function gtag(){dataLayer.push(arguments);}\
+  gtag('js', new Date());\
+  gtag('config', 'G-6DXRRLW54Y');\
+</script>\
+')
+
+
 //	<div class="container">\
 //		CKT - 2020\
 //	</div>\
